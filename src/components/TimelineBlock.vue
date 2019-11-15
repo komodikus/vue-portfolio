1b55d4e0bc3d05395f36340886c7203277e2822f
komodikus <marbel96@yandex.ru>
2019-11-15 13:47:22 +0300

    Add timeline block visual changes

--- src/components/TimelineBlock.vue
+++ src/components/TimelineBlock.vue
@@ -1,19 +1,45 @@
 <template>
   <div class="d-flex flex-wrap justify-content-center timeline">
-      <div class="work-block work-position"></div>
-      <div class="work-block work-position2"></div>
-      <div class="work-block work-position3"></div>
+    <Timeline
+      :timeline-items="dataTimeline"
+      :message-when-no-items="messageWhenNoItems"
+      :unique-year="true"
+      order="asc"
+      v-scroll-reveal.reset="{ delay: 350 }"
+    />
   </div>
 </template>
 
 
 <script>
+    import Timeline from 'timeline-vuejs'
     export default {
     name: 'timeline',
-    data() {
-        return {
-            
-        }
+    data: () => ({
+        messageWhenNoItems: 'There arent items',
+    dataTimeline: [
+      {
+        from: new Date(2017, 5),
+        title: "Name",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+      },
+      {
+        from: new Date(2017, 8),
+        title: "Name",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+      },
+      {
+        from: new Date(2016, 11),
+        title: "Name",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+      },
+    ]
+    }),
+    components:{
+        Timeline,
     },
     methods:{
         
@@ -23,18 +49,8 @@
 </script>
 
 <style  scoped>
-.work-block{
-    width: 320px;
-    height: 225px;
-}
-.work-position{
-    background: blue;
-}
-.work-position2{
-    background: red;
-}
-.work-position3{
-    background: green;
+.timeline{
+
 }
 </style>>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'vue-material-design-icons/styles.css';
+import '../node_modules/timeline-vuejs/dist/timeline-vuejs.css'
+import VueScrollReveal from 'vue-scroll-reveal';
+
 
 Vue.config.productionTip = false
-
+Vue.use(VueScrollReveal, {
+  class: 'v-scroll-reveal',
+  duration: 900,
+  scale: 1,
+  distance: '10px',
+  mobile: true
+});
 
 new Vue({
   render: h => h(App),
